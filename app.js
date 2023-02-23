2a108e8d36ee269c97edc70284edae0e203c1222
elionavarroh <elionavarroh@gmail.com>
2023-02-23 09:22:45 +0100

    format the app.js file

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ const product = { count: 3, price: 12.55, type: "ropa" };
 
 //cáculo del total por el precio
 let total;
-if(product.count > 0) {
+if (product.count > 0) {
     total = product.count * product.price;
-} else{
+} else {
     total = 0
 }
 console.log(total);
 
 //calculo del IVA según el tipo del producto
 let tipoIVA;
-switch(product.type){
+switch (product.type) {
     case "alimentacion": tipoIVA = 0.10; break;
     case "libro": tipoIVA = 0.04; break;
     default: tipoIVA = 0.21;
@@ -25,3 +25,4 @@ console.log(IVA)
 //calculo del total + el IVA
 let resultado;
 console.log(total + IVA)
+
