0f60dab4d3d416ffaea762c1d50bee92a9f90e31
elionavarroh <elionavarroh@gmail.com>
2023-02-23 18:11:33 +0100

    add a new section in the file app.js for differents variables on a salary example

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ if (product.count > 0) {
 }
 console.log(total);
 
-//calculo del IVA según el tipo del producto
+//cálculo del IVA según el tipo del producto
 let tipoIVA;
 switch (product.type) {
     case "alimentacion": tipoIVA = 0.10; break;
@@ -18,11 +18,43 @@ switch (product.type) {
 }
 console.log(tipoIVA);
 
-//calculo del IVA según el total
+//cálculo del IVA según el total
 let IVA = total * tipoIVA
 console.log(IVA)
 
-//calculo del total + el IVA
+//cálculo del total + el IVA
 let resultado;
 console.log(total + IVA)
 
+//cálculo sueldo bruto, número de hijos y número de pagas
+const empleado = {
+    bruto: 14500,
+    hijos: 2,
+    pagas: 14
+}
+
+//cáculo del porcentaje de retención, dependiendo también del número de hijos
+let retencion;
+if (empleado.bruto < 12000) {
+    retencion = empleado.bruto * (0 - (0.01 * empleado.hijos));
+} else if (empleado.bruto < 24000) {
+    retencion = empleado.bruto * (0.08 - (0.01 * empleado.hijos));
+} else if (empleado.bruto < 34000) {
+    retencion = empleado.bruto * (0.16 - (0.01 * empleado.hijos));
+} else {
+    retencion = empleado.bruto * (0.30 - (0.01 * empleado.hijos));
+}
+console.log(retencion);
+
+//cálculo del sueldo neto
+let sueldoneto = empleado.bruto - retencion
+console.log(sueldoneto);
+
+//cálculo del sueldo neto mensual
+let sueldonetomensual;
+if (sueldoneto > 14) {
+    sueldonetomensual = sueldoneto / 14
+} else {
+    sueldonetomensual = sueldoneto / 12
+}
+console.log(sueldonetomensual);
