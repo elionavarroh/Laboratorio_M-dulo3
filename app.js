5c7c30184cabc2a54995e737b544fd9678d9ec09
elionavarroh <elionavarroh@gmail.com>
2023-03-01 15:21:34 +0100

    Modified file app.js

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const product = { count: 3, price: 12.55, type: "ropa" };
 
 //cáculo del total por el precio
-let total;
+let total = 0;
 if (product.count > 0) {
     total = product.count * product.price;
 } else {
-    total = 0
+    total = 0;
 }
 console.log(total);
 
 //cálculo del IVA según el tipo del producto
-let tipoIVA;
+let tipoIVA = 0;
 switch (product.type) {
     case "alimentacion": tipoIVA = 0.10; break;
     case "libro": tipoIVA = 0.04; break;
@@ -19,22 +19,22 @@ switch (product.type) {
 console.log(tipoIVA);
 
 //cálculo del IVA según el total
-let IVA = total * tipoIVA
-console.log(IVA)
+let IVA = total * tipoIVA;
+console.log(IVA);
 
 //cálculo del total + el IVA
-let resultado;
-console.log(total + IVA)
+let resultado = 0;
+console.log(total + IVA);
 
 //cálculo sueldo bruto, número de hijos y número de pagas
 const empleado = {
     bruto: 14500,
     hijos: 2,
-    pagas: 14
-}
+    pagas: 14,
+};
 
 //cáculo del porcentaje de retención, dependiendo también del número de hijos
-let retencion;
+let retencion = 0;
 if (empleado.bruto < 12000) {
     retencion = empleado.bruto * (0 - (0.01 * empleado.hijos));
 } else if (empleado.bruto < 24000) {
@@ -47,14 +47,14 @@ if (empleado.bruto < 12000) {
 console.log(retencion);
 
 //cálculo del sueldo neto
-let sueldoneto = empleado.bruto - retencion
+let sueldoneto = empleado.bruto - retencion;
 console.log(sueldoneto);
 
 //cálculo del sueldo neto mensual
-let sueldonetomensual;
-if (sueldoneto > 14) {
-    sueldonetomensual = sueldoneto / 14
+let sueldonetomensual = 0;
+if (empleado.pagas >= 14) {
+    sueldonetomensual = sueldoneto / 14;
 } else {
-    sueldonetomensual = sueldoneto / 12
+    sueldonetomensual = sueldoneto / 12;
 }
 console.log(sueldonetomensual);
